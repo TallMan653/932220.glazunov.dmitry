c8a7cb4e5d26efad5765a730413140608723a160
Tall Man <pixellbvf@gmail.com>
2024-09-22 20:45:47 +0700

    lab6

--- scripts.js
+++ scripts.js
@@ -1,19 +1,20 @@
-var openedBlock;
-var fog;
-var isFogEnable;
+var elem;
+var picture;
 
-function openWin(name) {
-    openedBlock = document.getElementById(name);
-    openedBlock.style.display = "block";
-    fog = document.getElementById("fogBlock");
-    fog.style.display = "block";
-    isFogEnable = true;
-};
+function setColumn(blockId, pictureId, blockWidth, pictureWidth, displayPicture) {
+    elem = document.getElementById(blockId);
+    elem.style.width = blockWidth;
+    picture = document.getElementById(pictureId);
+    picture.style.display = displayPicture;
+    picture.style.width = pictureWidth;
+}
 
-function closeWin() {
-    if (isFogEnable) {
-        fog.style.display = "none";
-        isFogEnable = false;
-    }
-    openedBlock.style.display = "none";
+function openOne(b1, enabledPicture, b2, disabledPicture) {
+    setColumn(b1, enabledPicture, "850px", "600px", "block"); 
+    setColumn(b2, disabledPicture, "50px", "600px", "none");
+}
+
+function openTwo(b1, b2, pic1, pic2) {
+    setColumn(b1, pic1, "450px", "410px", "block");
+    setColumn(b2, pic2, "450px", "410px", "block");
 }
