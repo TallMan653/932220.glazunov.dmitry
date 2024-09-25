f58f8c24c3c88ea4726dba9e9fb08d2e065ec691
Tall Man <pixellbvf@gmail.com>
2024-09-25 11:02:42 +0700

    lab7

--- scripts.js
+++ scripts.js
@@ -1,20 +1,53 @@
-var elem;
-var picture;
+const OFFSET = 50; 
 
-function setColumn(blockId, pictureId, blockWidth, pictureWidth, displayPicture) {
-    elem = document.getElementById(blockId);
-    elem.style.width = blockWidth;
-    picture = document.getElementById(pictureId);
-    picture.style.display = displayPicture;
-    picture.style.width = pictureWidth;
+function createFigure(figureName){
+    var numOfFigures = document.getElementById("numberOfFigures").value;
+    for (let i=0; i < numOfFigures; i++){
+        let div = document.createElement('div');
+        div.className = 'Shape';
+
+        let size = Math.random() * 300 + 5;
+        let heightOfWindow = document.documentElement.clientHeight - size - OFFSET - 10;
+        let widthOfWindow = document.documentElement.clientWidth - size - 10;
+
+        div.style.width = size + 'px';
+        div.style.height = size + 'px';
+        
+        div.style.top = Math.random() * heightOfWindow + OFFSET + 'px';
+        div.style.left = Math.random() * widthOfWindow + 'px';
+        
+        div.style.border = '1px solid black';
+
+        SelectFigure(figureName, div, size);
+
+        document.body.append(div);
+
+        div.onclick = function(){
+            div.style.background = "yellow";
+            div.style.opacity = '80%';
+        };
+        div.ondblclick = function(){div.remove()};
+    }
 }
 
-function openOne(b1, enabledPicture, b2, disabledPicture) {
-    setColumn(b1, enabledPicture, "850px", "600px", "block"); 
-    setColumn(b2, disabledPicture, "50px", "600px", "none");
-}
-
-function openTwo(b1, b2, pic1, pic2) {
-    setColumn(b1, pic1, "450px", "410px", "block");
-    setColumn(b2, pic2, "450px", "410px", "block");
+function SelectFigure(figureName, div, size) {
+    switch (figureName) {
+        case "Circle":
+            div.style.borderRadius = size / 2 + 'px';
+            div.style.background = 'green';
+            break;
+        case "Square":
+            div.style.background = 'red';
+            div.style.width = size + 'px'; // Задаем ширину и высоту для квадрата
+            div.style.height = size + 'px';
+            break;
+        case "Triangle":
+            div.style.border = size / 2 + "px solid transparent";
+            div.style.borderBottom = size / 2 + "px solid blue";
+            div.style.width = "0px";
+            div.style.height = "0px";
+            break;
+        default:
+            break;
+    }
 }
